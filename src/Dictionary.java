164970b920b2732b8703fb2a3956faea7eab3dae
Evgeniy Golubtsov <e.golubtsoff@gmail.com>
2018-01-28 15:19:17 +0700

    ver.1.2

--- src/Dictionary.java
+++ src/Dictionary.java
@@ -7,49 +7,50 @@ public class Dictionary {
     final static String testString = "сапог сарай арбуз болт болт бокс биржа";
     private final Map<String, List<String>> dictionary = new TreeMap<>();
 
-    public Dictionary(String source){
+    public Dictionary(String source) {
         Arrays.stream(source.split("\\PL+"))
                 .map(String::toLowerCase)
                 .distinct()
                 .forEach(s -> {
-                    String letter = s.substring(0,1);
-                    if (!dictionary.containsKey(letter)){
+                    String letter = s.substring(0, 1);
+                    if (!dictionary.containsKey(letter)) {
                         List<String> list = new ArrayList<>();
                         list.add(s);
                         dictionary.put(letter, list);
                     } else {
-                        List<String> list = dictionary.get(letter);
-                        list.add(s);
-                        dictionary.put(letter, list);
+                        dictionary.get(letter).add(s);
                     }
                 });
     }
 
+    // for quick test
     public static void main(String[] args) {
         Dictionary dictionary = new Dictionary(testString);
-        System.out.println(dictionary.toStringGroupsWithMultipleElements());
+        System.out.println("Source test string: " + "сапог сарай арбуз болт болт бокс биржа");
+        System.out.println("Structure of dictionary: " + dictionary.toString());
+        System.out.println("Only sorted groups with several elements: " + dictionary.toStringGroupsWithMultipleElements());
     }
 
     @Override
-    public String toString(){
+    public String toString() {
         return dictionary.toString();
     }
 
-    public String toStringGroupsWithMultipleElements(){
+    public String toStringGroupsWithMultipleElements() {
         Map<String, List<String>> result = new TreeMap<>();
         dictionary.forEach((k, v) -> {
-            if (v.size() > 1){
+            if (v.size() > 1) {
                 result.put(k, sortValues(v));
             }
         });
         return result.toString();
     }
 
-    private List<String> sortValues(List<String> source){
+    private List<String> sortValues(List<String> source) {
         List<String> result = new ArrayList<>();
         result.addAll(source);
         result.sort((s1, s2) -> {
-            if (s1.length() == s2.length()){
+            if (s1.length() == s2.length()) {
                 return s1.compareTo(s2);
             } else {
                 return s2.length() - s1.length();
